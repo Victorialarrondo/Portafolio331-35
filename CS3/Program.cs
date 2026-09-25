cc52aaf245609a22d0356b4ced9926b1b351f037
Victorialarrondo <a.194585@imbc.edu.mx>
2026-09-25 09:27:42 -0700

    Add files via upload

--- CS3/Program.cs
+++ CS3/Program.cs
@@ -1,1 +1,26 @@
+﻿using System;
 
+namespace CS3 {
+    class Program {
+        static void Main(string[] args) {
+            double a = 1;
+            double b = 2;
+            double resultado = 0;
+
+            resultado = a + b;
+            Console.WriteLine($"Suma: {resultado}");
+            resultado = a - b;
+            Console.WriteLine($"Resta: {resultado}");
+            resultado = a * b;
+            Console.WriteLine($"Multiplicacion: {resultado}");
+            resultado = a / b;
+            Console.WriteLine($"Division: {resultado}");
+            resultado = a % b;
+            Console.WriteLine($"Residuo: {resultado}");
+
+            bool m = false;
+            m = 4 == 10;
+            Console.WriteLine($"Igualdad: {m}");
+        }
+    }
+}
